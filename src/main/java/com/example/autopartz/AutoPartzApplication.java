75464a67ea2f22596aa294c59f56acaa4dfa19c1
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2022-12-27 01:36:08 +0100

    Initializing the app

---
+++ src/main/java/com/example/autopartz/AutoPartzApplication.java
@@ -0,0 +1,13 @@
+package com.example.autopartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoPartzApplication.class, args);
+    }
+
+}
