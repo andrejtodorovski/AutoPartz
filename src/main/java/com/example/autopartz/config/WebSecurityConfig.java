c79897fe7d1636c1dadb63a832434a111d670825
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-03-18 23:33:54 +0100

    Sredeni zabeleshki po diskusii

--- src/main/java/com/example/autopartz/config/WebSecurityConfig.java
+++ src/main/java/com/example/autopartz/config/WebSecurityConfig.java
@@ -27,8 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/products", "/services", "/filtered", "/login", "/register","/registerWarehouseman","/finishRegister","/test/*","/access_denied","/carCategoryReport","/partManufacturersReport","/mostPurchasedPart").permitAll()
-                .antMatchers("/orders","/repairs","/reviews","/part/*","/currentOrder","/addCarSampleForUser","/repairs/addReview/*").hasRole("CLIENT")
+                .antMatchers("/", "/products", "/services", "/filtered", "/login", "/register","/registerWarehouseman","/finishRegister","/test/*","/access_denied","/carCategoryReport","/partManufacturersReport","/mostPurchasedPart","/part/*").permitAll()
+                .antMatchers("/orders","/repairs","/reviews","/currentOrder","/addCarSampleForUser","/repairs/addReview/*").hasRole("CLIENT")
                 .antMatchers("/viewUsers","/approve/*","/addPart","/addCarManufacturer","/addPartManufacturer","/addCategory","/addCar","/addRepairShop","/addWarehouse").hasRole("ADMIN")
                 .antMatchers("/myWarehouseReport","myWarehouse").hasRole("WAREHOUSEMAN")
                 .anyRequest()
